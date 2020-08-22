6dcee487b9b05f8904b8a27b57373fd4e3e7c030
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-08-22 00:48:13 -0500

    Updates

---
+++ precache-manifest.f739122d8e92e776665004a6b2a3046e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1f7ad9dc854b88ee1031cf54a00e6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4ada661aeb08148deed",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c9cec222d6bffda62d8",
+    "url": "/static/js/2.6782a6dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6782a6dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4ada661aeb08148deed",
+    "url": "/static/js/main.6dc912e1.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
