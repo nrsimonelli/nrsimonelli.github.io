2f5a1d94cc1d72490cd566416192d2b7b86d3f43
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-08-22 01:01:12 -0500

    react setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='root'>
+      <p>hello</p>
+      
+      
+    </div>
+  );
+}
+
+export default App;
