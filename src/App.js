976729b9aae59deed59aa7e70182358b761e8de6
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-08-23 12:45:06 -0500

    moved onto interests and proceeding to professional

--- src/App.js
+++ src/App.js
@@ -4,7 +4,63 @@ import './App.css';
 function App() {
   return (
     <div className='root'>
-      <p>hello</p>
+      <div className='top'>
+        <div className='top-sub left'>ham</div>
+        <div className='top-sub center'>NICHOLAS SIMONELLI</div>
+        <div className='top-sub right'>Icon</div>
+      </div>
+        <div className='sector one'>
+          <div className='sector one layer'>
+          <div>Movement Specialist</div>
+          <div>Full Stack Developer</div>
+
+          </div>
+          
+        </div>
+        <div className='container'>
+          <div className='sector two'>
+            <div className='two text title'><a href='https://www.youtube.com/watch?v=qsxNIW97RqA' target='blank'>Sub Zero</a></div>
+            <div className='two text title'><a href='https://subzeroultimate.com' target='blank'>Minneapolis Ultimate Club</a></div>
+          </div>
+        </div>
+        
+        <div className='sector three'>
+          <div className='box'>
+            <div className='three text music'>
+              <div className='card card-title'>Discovery, Daft Punk</div>
+              <div className='card card-description'></div>
+              <div className='card card-info'><a href='https://www.youtube.com/watch?v=PTt_G1Ft3yA' target='blank'>More Info</a></div>
+            </div>
+          </div>
+          <div className='box'>
+            <div className='three text thriller'>
+              <div className='card card-title'>Enemy by Denis Villeneuve</div>
+              <div className='card card-description'></div>
+              <div className='card card-info'><a href='https://www.youtube.com/watch?v=r98msNRZYUY' target='blank'>More Info</a></div>
+            </div>
+          </div>
+          
+          <div className='box'>
+            <div className='three text game'>
+              <div className='card card-title'>Scythe, The Board Game</div>
+              <div className='card card-description'></div>
+              <div className='card card-info'><a href='https://belovedpacifist.com/' target='blank'>More Info</a></div>
+            </div>
+          </div>
+          
+
+        </div>
+        <div className='sector four'>
+        <div>sub title</div>
+
+        </div>
+        <div className='sector five'>
+          <div>sub title</div>
+
+        </div>
+        <p>hello</p>
+        <footer>bye</footer>
+      
       
       
     </div>
