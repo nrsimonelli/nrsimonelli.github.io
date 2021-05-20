5de72401994dfbd634cdf1715f1763de4d582a7f
nrsimonelli <nrsimonelli@gmail.com>
2021-05-20 15:07:15 -0500

    added image color specificity

--- src/Projects.js
+++ src/Projects.js
@@ -6,7 +6,7 @@ const Projects = () => {
       <div className='project-container container'>
         <div className='project-title'>Works</div>
         <div className='project-card-container break-rev'>
-          <div className='project-card-image wave-2'></div>
+          <div className='project-card-image project-1 wave-stack'></div>
           <div className='project-card-detail'>
             <div className='project-card-detail-title'>
               Heroic Data Vision
@@ -20,7 +20,35 @@ const Projects = () => {
           </div>
         </div>
         <div className='project-card-container break-reg'>
-          <div className='project-card-image wave-3'></div>
+          <div className='project-card-image project-2'></div>
+          <div className='project-card-detail'>
+            <div className='project-card-detail-title'>
+              Dark Magic
+            </div>
+            <div className='project-card-detail-subtitle'>
+              Web Design, Sass, Colors
+            </div>
+            <a className='project-card-button button' href='/'>
+              View
+            </a>
+          </div>
+        </div>
+        <div className='project-card-container break-rev'>
+          <div className='project-card-image project-3'></div>
+          <div className='project-card-detail'>
+            <div className='project-card-detail-title'>
+              Dark Magic
+            </div>
+            <div className='project-card-detail-subtitle'>
+              Web Design, Sass, Colors
+            </div>
+            <a className='project-card-button button' href='/'>
+              View
+            </a>
+          </div>
+        </div>
+        <div className='project-card-container break-reg'>
+          <div className='project-card-image project-4'></div>
           <div className='project-card-detail'>
             <div className='project-card-detail-title'>
               Dark Magic
