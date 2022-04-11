0eeeb5204bcdac04818f2dff3aeef33c473ff44f
nrsimonelli <nrsimonelli@gmail.com>
2022-04-11 15:16:03 -0500

    gitignore and build update

---
+++ build/precache-manifest.189974fbfd5d7d661494f22892c76b46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87df6e43280f143d01c8b8363de2f5a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4c0d761994f763ad688",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "8f8618b49632b5cebc24",
+    "url": "/static/css/main.107ee5c7.chunk.css"
+  },
+  {
+    "revision": "c4c0d761994f763ad688",
+    "url": "/static/js/2.31beb629.chunk.js"
+  },
+  {
+    "revision": "6a29b30dd9e8ef5706396c026cb51452",
+    "url": "/static/js/2.31beb629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f8618b49632b5cebc24",
+    "url": "/static/js/main.010696d7.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  }
+]);
